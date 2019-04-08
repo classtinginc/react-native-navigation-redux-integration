ea471b41b4aa4ee08756e1d3dbef1956feac8481
Hong sa sung <scvhss@naver.com>
2019-04-08 21:00:00 +0900

    Update listeners.js
    exception되는 screen 추가

--- src/listeners.js
+++ src/listeners.js
@@ -1,3 +1,4 @@
+/* eslint linebreak-style: off */
 import {
   rootSet,
   screenDisappeared,
@@ -13,15 +14,17 @@ import {
   modalDismissed,
   allModalsDismissed,
   overlayShown,
-  overlayDismissed
+  overlayDismissed,
 } from './actions';
-import {processRoot, processLayout} from './helpers';
-import {Events} from './constants';
+import { processRoot, processLayout } from './helpers';
+import { Events } from './constants';
 
 let commandListener = null;
-let navigationButtonEventListener = null;
+const navigationButtonEventListener = null;
 let bottomTabEventListener = null;
 let screenDidAppearEventListener = null;
+
+const exceptScreen = ['Classting.WritePost', 'Classting.WriteNoticeBoard', 'Classting.AlbumDetail'];
 
 const subscribeCommandListener = (navigator, store) => {
   commandListener = navigator
@@ -29,53 +32,52 @@ const subscribeCommandListener = (navigator, store) => {
     .registerCommandListener((name, params) => {
       switch (name) {
         case Events.setRoot:
-          {
-            const initState = processRoot(params.layout);
-            return store.dispatch(rootSet(initState))
-          }
+        {
+          const initState = processRoot(params.layout);
+          return store.dispatch(rootSet(initState));
+        }
         case Events.push:
           const componentId = params.componentId;
           const layout = processLayout(params.layout);
-          return store.dispatch(screenPushed({componentId, layout}));
+          return store.dispatch(screenPushed({ componentId, layout }));
         case Events.pop:
-          return store.dispatch(screenPopped({componentId: params.componentId}));
+          return store.dispatch(screenPopped({ componentId: params.componentId }));
         case Events.popToRoot:
-          return store.dispatch(stackPoppedToRoot({componentId: params.componentId}));
+          return store.dispatch(stackPoppedToRoot({ componentId: params.componentId }));
         case Events.popTo:
-          return store.dispatch(poppedToScreen({componentId: params.componentId}));
+          return store.dispatch(poppedToScreen({ componentId: params.componentId }));
         case Events.setStackRoot:
-          {
-            const componentId = params.componentId;
-            const layout = params
-              .layout
-              .map(layout => processLayout(layout));
-            return store.dispatch(stackRootSet({componentId, layout}));
-          }
+        {
+          const componentId = params.componentId;
+          const layout = params
+            .layout
+            .map(layout => processLayout(layout));
+          return store.dispatch(stackRootSet({ componentId, layout }));
+        }
         case Events.mergeOptions:
-          {
-            if (params && params.options && params.options.bottomTabs && (params.options.bottomTabs.currentTabIndex !== undefined || params.options.topTabs.currentTabIndex !== undefined)) {
-              return store.dispatch(tabChangedWithMergeOptions({componentId: params.componentId, currentTabIndex: params.options.bottomTabs.currentTabIndex}));
-            }
-            return;
+        {
+          if (params && params.options && params.options.bottomTabs && (params.options.bottomTabs.currentTabIndex !== undefined || params.options.topTabs.currentTabIndex !== undefined)) {
+            return store.dispatch(tabChangedWithMergeOptions({ componentId: params.componentId, currentTabIndex: params.options.bottomTabs.currentTabIndex }));
           }
+          return;
+        }
         case Events.showModal:
           const modalLayout = processLayout(params.layout);
           return store.dispatch(modalShown(modalLayout));
         case Events.dismissModal:
-          return store.dispatch(modalDismissed({componentId: params.componentId}));
+          return store.dispatch(modalDismissed({ componentId: params.componentId }));
         case Events.dismissAllModals:
           return store.dispatch(allModalsDismissed());
         case Events.showOverlay:
-          {
-            const overlayLayout = processLayout(params.layout);
-            return store.dispatch(overlayShown(overlayLayout));
-          }
+        {
+          const overlayLayout = processLayout(params.layout);
+          return store.dispatch(overlayShown(overlayLayout));
+        }
         case Events.dismissOverlay:
-          return store.dispatch(overlayDismissed({componentId: params.componentId}));
+          return store.dispatch(overlayDismissed({ componentId: params.componentId }));
         default:
-          return;
       }
-    })
+    });
 };
 
 const subscribeToBottomTabEvent = (navigator, store) => {
@@ -83,35 +85,39 @@ const subscribeToBottomTabEvent = (navigator, store) => {
     .events()
     .registerBottomTabSelectedListener((params) => {
       store.dispatch(tabChanged(params));
-    })
-}
+    });
+};
 
 const subscribeToScreenDidAppear = (navigator, store) => {
   screenDidAppearEventListener = navigator
     .events()
-    .registerComponentDidAppearListener(({componentId, componentName}) => {
-      store.dispatch(screenAppeared({componentId, componentName}));
-    })
-}
+    .registerComponentDidAppearListener(({ componentId, componentName }) => {
+      if (!exceptScreen.includes(componentName)) {
+        store.dispatch(screenAppeared({ componentId, componentName }));
+      }
+    });
+};
 
 const subscribeToScreenDisappear = (navigator, store) => {
   screenDisappearEventListener = navigator
     .events()
-    .registerComponentDidDisappearListener(({componentId, componentName}) => {
-      store.dispatch(screenDisappeared({componentId, componentName}));
-    })
-}
+    .registerComponentDidDisappearListener(({ componentId, componentName }) => {
+      if (!exceptScreen.includes(componentName)) {
+        store.dispatch(screenDisappeared({ componentId, componentName }));
+      }
+    });
+};
 
 export const subscribeToEvents = (navigator, store) => {
   subscribeCommandListener(navigator, store);
   subscribeToBottomTabEvent(navigator, store);
   subscribeToScreenDisappear(navigator, store);
   subscribeToScreenDidAppear(navigator, store);
-}
+};
 
 export const unsubscribe = () => {
   commandListener && commandListener.remove();
   navigationButtonEventListener && navigationButtonEventListener.remove();
   bottomTabEventListener && bottomTabEventListener.remove();
   screenDidAppearEventListener && screenDidAppearEventListener.remove();
-}
+};
